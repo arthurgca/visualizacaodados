7c54efb048c1e7289dd225cf66f27a372d2673c0
Arthur Gurjao <arthur.azevedo@ccc.ufcg.edu.br>
2017-02-23 20:26:41 -0300

    Mudança para Github Pages'

---
+++ js/example.js
@@ -0,0 +1,12 @@
+
+d3.json('data/data.json', function(err, data) {
+var chart = radialBarChart()
+  .barHeight(250)
+  .domain([0,7])
+  .tickCircleValues([0,1,2,3,4,5,6])
+  .capitalizeLabels(true)
+
+d3.select('#chart')
+  .datum(data)
+  .call(chart);
+});
